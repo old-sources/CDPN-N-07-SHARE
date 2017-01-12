8231ea3649c4375b722d50c491cf1bdda853e180
WoufWouf <ludovic.garraud@gmail.com>
2017-01-12 16:29:49 +0100

    Full stack sources + frontal update

---
+++ crowdfunding-persistence/src/persistence/Personne.java
@@ -0,0 +1,90 @@
+package persistence;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the "personne" database table.
+ * 
+ */
+@Entity
+@Table(name="personne")
+@NamedQuery(name="Personne.findAll", query="SELECT p FROM Personne p")
+public class Personne implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="datenaiss")
+	private Date datenaiss;
+
+	@Column(name="login")
+	private String login;
+
+	@Column(name="mdp")
+	private String mdp;
+
+	@Column(name="nom")
+	private String nom;
+
+	@Column(name="prenom")
+	private String prenom;
+
+	public Personne() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDatenaiss() {
+		return this.datenaiss;
+	}
+
+	public void setDatenaiss(Date datenaiss) {
+		this.datenaiss = datenaiss;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMdp() {
+		return this.mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+}
